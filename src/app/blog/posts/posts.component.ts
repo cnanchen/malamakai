4dcdb05e67f64f92109446e265e57b9cc84ac461
david mimay <davidmimay@gmail.com>
2022-02-11 17:56:54 +0000

    ✏️ dynamic blogger finish

--- src/app/blog/posts/posts.component.ts
+++ src/app/blog/posts/posts.component.ts
@@ -1,28 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { environment } from '../../../environments/environment';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DomSanitizer } from '@angular/platform-browser';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 import { BloggerService } from '../blogger.service';
-import { hasLifecycleHook } from '@angular/compiler/src/lifecycle_reflector';
 
 @Component({
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent implements OnInit {
 
-  pageId: string;
-  playlistUrl: string;
-  safeUrl: any;
-  githubUrl: string;
-  blogId: string = '6664790489593253867'; // TO-DO your blog ID.
+export class PostsComponent {
 
   posts: any[] = [];
-
-  private youtubeUrl:string = 'https://www.googleapis.com/blogger/v3'
-  private apikey:string = environment.firebaseConfig.apiKey;
 
   constructor(
     private route: ActivatedRoute,
@@ -30,51 +19,7 @@ export class PostsComponent implements OnInit {
     public http:HttpClient,
     public bloggerService: BloggerService,
   ) {
-    this.pageId = this.route.snapshot.paramMap.get('id');
     this.bloggerService.getBloggerPosts().subscribe(posts => this.posts = posts);
-
-    //this.playlistUrl = `https://www.youtube.com/embed/videoseries?list=${this.playlistId}`;
-    // this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.playlistUrl);
-    //this.githubUrl = `https://www.github.com/ethtomars/${this.playlistId}`;
-    
-    //this.getPages().subscribe(videos => this.videos = videos);
-  }
-
-  
-  ngOnInit() {
-    let hello = 0;
-    // this.getPages().subscribe(videos => this.videos = videos);
-  }
-  
-
-  // SAMPLE PAGES: https://www.googleapis.com/blogger/v3/blogs/4967929378133675647/pages?key=YOUR-API-KEY
-  // SPECIFIC PAGE: https://www.googleapis.com/blogger/v3/blogs/4967929378133675647/pages/273541696466681878?key=YOUR-API-KEY
-
-  // SAMPLE POST: https://www.googleapis.com/blogger/v3/blogs/2399953/posts?key=YOUR-API-KEY
-
-  getPages() {
-    let url = `${this.youtubeUrl }/blogs/${this.blogId}/pages/${this.pageId}`;
-    let params = new HttpParams();
-
-    //params = params.append('part', 'snippet');
-    //params = params.append('maxResults', '100');
-    // params = params.append('/pages', this.pageId);
-    //params = params.append('blogs', this.blogId);
-    // params = params.append('pages', '');
-    params = params.append('key', this.apikey);
-
-    return this.http.get( url, { params } ).pipe( map( (res: any) => {
-      console.log('📋 Blog', res);
-      let videos = res;
-      /*
-      for ( let video of res.items ) {
-        let snippet = video; // let snippet = video.snippet;
-        videos.push( snippet );
-      }
-      */
-      return videos;
-    }) );
-
   }
 
 }
